2da0795536478b9a5526d2ed1d760394b3a3df79
bhujay <bhujay.bhatta@yahoo.com>
2018-12-02 19:02:33 +0530

    blue print from a folder named view

--- app1/views/routes.py
+++ app1/views/routes.py
@@ -1,9 +1,5 @@
-# import app1
-# # from app1 import flask_app_var
-# 
-# app = app1.create_app()
-
-#@flask_app_var.route('/')
-@app.route('/')
+from app1.views import views_bp
+@views_bp.route('/world')
 def hello():
     return 'Hello World'   
+
--- app_run.py
+++ app_run.py
@@ -3,5 +3,6 @@ import app1
 # from app1.views import views_bp
 # bps = [views_bp,]
 from app1 import auth
-bp_list = [auth.bp,]
+from app1.views import views_bp
+bp_list = [auth.bp,views_bp]
 app = app1.create_app(blue_print_list=bp_list)
