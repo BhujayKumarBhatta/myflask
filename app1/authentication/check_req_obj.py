522a93f492f9db56b00c1051850c54e5be43c5a7
bhujay <bhujay.bhatta@yahoo.com>
2018-12-03 15:37:21 +0530

    check request object

--- app1/authentication/check_req_obj.py
+++ app1/authentication/check_req_obj.py
@@ -3,10 +3,10 @@ from flask.globals import session
 import json
 #from flask.views import MethodView
 
-token_bp = Blueprint('tokenops', __name__)
+req_obj_bp = Blueprint('check_req_obj', __name__)
 
-@token_bp.route('/check-req-obj', methods=['GET', 'POST'])
-def get_token():
+@req_obj_bp.route('/check-req-obj', methods=['GET', 'POST'])
+def check_req_obj():
     '''
     check request object  
     curl -X POST --header "Content-Type: application/json"  -d '{"username": "bhujay"}' localhost:5000/token
@@ -25,7 +25,25 @@ def get_token():
     request.data = b'{"username": "bhujay"}'
     request.form = ImmutableMultiDict([])
     jsonify(request.form) = <Response 3 bytes [200 OK]>
-    request.get_data() = b'{"username": "bhujay"}'   
+    request.get_data() = b'{"username": "bhujay"}'
+    
+    
+        'accept_charsets', 'accept_encodings', 'accept_languages', 'accept_mimetypes', 'access_route',
+    'application', 'args', 'authorization', 'base_url', 'blueprint', 'cache_control',
+    'charset', 'close', 'content_encoding', 'content_length', 'content_md5', 'content_type',
+    'cookies', 'data', 'date', 'dict_storage_class', 'disable_data_descriptor',
+    'encoding_errors', 'endpoint', 'environ', 'files',
+    'form', 'form_data_parser_class', 'from_values', 'full_path',
+    'get_data', 'get_json', 'headers', 'host', 'host_url',
+    'if_match', 'if_modified_since', 'if_none_match', 'if_range', 'if_unmodified_since',
+    'input_stream', 'is_json', 'is_multiprocess', 'is_multithread',
+    'is_run_once', 'is_secure', 'is_xhr', 'json', 'list_storage_class',
+    'make_form_data_parser', 'max_content_length', 'max_form_memory_size', 'max_forwards',
+    'method', 'mimetype', 'mimetype_params', 'module', 'on_json_loading_failed',
+    'parameter_storage_class', 'path', 'pragma', 'query_string', 'range', 'referrer',
+    'remote_addr', 'remote_user', 'routing_exception', 'scheme', 'script_root',
+    'shallow', 'stream', 'trusted_hosts', 'url', 'url_charset', 'url_root', 'url_rule',
+    'user_agent', 'values', 'view_args', 'want_form_data_parsed']"   
     '''
     if  request.method == 'POST' or request.method == 'GET':
         inside_request_object = ("request.json = %s \n" 
@@ -37,6 +55,9 @@ def get_token():
                                  "jsonify(request.values) = %s \n"
                                  "request.headers = %s \n" 
                                  "request.environ = %s \n"
+                                 "request.authorization = %s \n"
+                                 "request.endpoint = %s \n"
+                                 "request.application = %s \n"
                                  "session = %s \n"
                                  
                                  % (request.json,
@@ -48,6 +69,9 @@ def get_token():
                                     jsonify(request.values),
                                     request.headers,
                                     request.environ,
+                                    request.authorization,
+                                    request.endpoint,
+                                    request.application,
                                     session
                                     )
                                  )
--- app_run.py
+++ app_run.py
@@ -4,6 +4,6 @@ import app1
 # bps = [views_bp,]
 from app1 import auth
 from app1.views import views_bp
-from app1.authentication.tokenops import token_bp
-bp_list = [auth.bp,views_bp, token_bp]
+from app1.authentication.check_req_obj import req_obj_bp
+bp_list = [auth.bp,views_bp, req_obj_bp]
 app = app1.create_app(blue_print_list=bp_list)
