1b9f1f13ebe8554b1c6bdff193f0b1d57da5f8f3
bhujay <bhujay.bhatta@yahoo.com>
2018-12-04 15:47:00 +0530

    app.config have the private key now

---
+++ app1/configs/configs.py
@@ -0,0 +1,30 @@
+import os
+import konfig
+
+_HERE = os.path.dirname(__file__)
+_SETTINGS_FILE = os.path.join(_HERE, 'settings.ini')
+
+CONFS = konfig.Config(_SETTINGS_FILE)
+flask_default_setiings_map = CONFS.get_map('flask_default')
+token_settings_map = CONFS.get_map('token')
+
+if token_settings_map.get('private_key_file_location') == 'default':
+    private_key_filename = os.path.expanduser('~/.ssh/id_rsa')
+else:
+    private_key_filename = token_settings_map.get('private_key_file_location')
+    
+if token_settings_map.get('public_key_file_location') == 'default':
+    public_key_filename = os.path.expanduser('~/.ssh/id_rsa.pub')
+else:
+    public_key_filename = token_settings_map.get('public_key_file_location')
+
+with open(private_key_filename, 'r') as f:
+        private_key = f.read()
+with open(public_key_filename, 'r') as f:
+        public_key = f.read()      
+key_attr = {'private_key': private_key, 'public_key': public_key}
+token_settings_map.update(key_attr)
+
+prod_configs_from_file = [flask_default_setiings_map,
+                        token_settings_map,]
+
--- app_run.py
+++ app_run.py
@@ -1,25 +1,12 @@
 # from app1 import flask_app_var
-import os
-import konfig
+
 import app1
-# from app1.views import views_bp
-# bps = [views_bp,]
-from app1 import auth
-from app1.views import views_bp
+from app1.configs import configs 
+
 from app1.authentication.check_req_obj import req_obj_bp
 from app1.authentication.tokenops import token_bp
 
-_HERE = os.path.dirname(__file__)
-_SETTINGS_FILE = os.path.join(_HERE, 'settings.ini')
+bp_list = [req_obj_bp,token_bp,]
 
-CONFS = konfig.Config(_SETTINGS_FILE)
-flask_default_setiings_map = CONFS.get_map('flask_default')
-token_settings_map = CONFS.get_map('token')
-prod_configs = [flask_default_setiings_map,
-                        token_settings_map,]
-
-
-bp_list = [auth.bp,views_bp, req_obj_bp,token_bp]
-
-app = app1.create_app(config_map_list= prod_configs,
+app = app1.create_app(config_map_list= configs.prod_configs_from_file,
                       blue_print_list=bp_list)
